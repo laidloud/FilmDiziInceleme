e2b5e6f92e90ac002b2f7f47bc7cf3bad70996e7
Windows <Windows@MONSTER>
2025-12-24 15:57:30 +0300

    Initial commit

---
+++ FilmDiziInceleme/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace FilmDiziInceleme.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Film> Films { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ FilmDiziInceleme/Models/Film.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace FilmDiziInceleme.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Type { get; set; } // Film/Dizi
+
+        public string Genre { get; set; }
+        
+        public int Year { get; set; }
+
+        [Range(0, 10)]
+        public float Rating { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+    }
+}
